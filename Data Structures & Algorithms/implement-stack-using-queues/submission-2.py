a1af366f8734d31b6b48577e4ae14f9a7f5a67ea
Scott Powers <spowers.42@gmail.com>
2026-05-11 11:10:14 -0400

    Add: implement-stack-using-queues - submission-2

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-2.py
@@ -0,0 +1,66 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        self.head = Node(-1)
+        self.tail = self.head
+        self.size = 0
+
+    def push(self, value):
+        node = Node(value)
+        self.tail.next = node
+        self.tail = self.tail.next
+        self.size += 1
+
+    def peak(self):
+        if self.size > 0:
+            return self.head.next.value
+        return None
+
+    def pop(self):
+        if self.size > 0:
+            target = self.head.next
+            self.head.next = target.next
+            if self.size == 1:
+                self.tail = self.head
+            self.size -= 1
+            return target.value
+        return None
+
+    def empty(self):
+        return self.size == 0
+
+
+class MyStack:
+    def __init__(self):
+        self.active = Queue()
+        self.working = Queue()
+
+    def push(self, x: int) -> None:
+        self.working.push(x)
+        while self.active.size > 0:
+            self.working.push(self.active.pop())
+        tmp = self.active
+        self.active = self.working
+        self.working = tmp
+
+    def pop(self) -> int:
+        return self.active.pop()
+
+    def top(self) -> int:
+        return self.active.peak()
+
+    def empty(self) -> bool:
+        return self.active.empty()
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
